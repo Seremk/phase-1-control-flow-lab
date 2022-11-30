37ab990378398f77188eab160f28d048bdd3207e
seremshadrack <skemei660@gmail.com>
2022-11-30 20:01:19 +0300

    completed

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return "This one is on me!";
+  }
+  else if (feet > 2000 && feet < 2500) {
+    return "I will gladly take your thirty bucks.";
+  }
+  else {
+    return "No can do.";
+  }
+}
+
+// function ternaryCheckCity(destination){
+  // let result;
+  //  destination === 'NYC'? 
+    // result "Ok, sounds good."; result "No go."
+function ternaryCheckCity(city) {
+  return city=="NYC" ? "Ok, sounds good." : "No go.";
+   console.log (pittsbourgh)
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip) {
+  let result;
+  switch(tip){
+         case 'generous':
+         result = "Thank you so much.";
+      break;
+   case 'not as generous':
+   result= "Thank you.";
+      break;
+    default:
+      result= "Bye.";
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+   }
+    return result;
+   }
